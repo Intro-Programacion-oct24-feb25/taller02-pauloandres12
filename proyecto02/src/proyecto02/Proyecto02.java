1a976a8158aedaf62edec4a9f603274b1569a6b3
Paulo Bustamante <pbustamante@utpl.edu.ec>
2024-10-17 11:18:38 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author utpl
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo dos basico de Java Versionado a traves de Git Manejado desde Github Ademas se usar GitHubClassroom");
+        
+    }
+    
+}
